53cb4aa014aa439ce192a441d314e2aa383f2fbc
Adam Gliński <adam@MacBook-Pro-Adam.local>
2024-12-09 21:03:36 +0100

    Fixed timeout, added documentation
    and inited project_demo.sh script

--- src/Slave.java
+++ src/Slave.java
@@ -1,6 +1,8 @@
 
 import java.io.IOException;
 import java.net.SocketException;
+import java.net.SocketTimeoutException;
+import java.sql.SQLOutput;
 
 public class Slave {
     private static boolean foundPort = false;
@@ -10,6 +12,7 @@ public class Slave {
     public static void work(int destPort, int numberToSend) {
         int port = MIN_PORT;
         int maxRetry = 5;
+        int timeout = 2000;
         while (!foundPort) {
             try (UDPManager manager = new UDPManager(port)) {
                 foundPort = true;
@@ -17,15 +20,20 @@ public class Slave {
                     manager.setDestHost("localhost");
                     manager.setDestPort(destPort);
                     manager.send(Integer.toString(numberToSend));
-                    manager.getSocket().setSoTimeout(2000);
-
-                    String resp = manager.receive();
-                    if (resp.equals("OK")) break;
+                    manager.getSocket().setSoTimeout(timeout);
+                    try {
+                        String resp = manager.receive();
+                        if (resp.equals("OK")) break;
+                    } catch (SocketTimeoutException e) {
+                        System.err.println(retryCounter == maxRetry - 1 ? "Socket timed out, failed to send information." : "Socket timed out, retrying!");
+                    }
                 }
-                } catch (SocketException e) {
+            }
+            catch (SocketException e) {
                 System.out.println("Failed to open port " + port + ". Looking for a new one...");
                 port = MIN_PORT + (int)(Math.random() * ((MAX_PORT - MIN_PORT) + 1));
             } catch (IOException e) {
+                System.out.println("test");
                 System.out.println(e.getMessage());
             }
         }
